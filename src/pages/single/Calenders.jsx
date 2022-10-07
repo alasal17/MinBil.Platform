f997aca63b05e38af6a0ec362d903ebc30bbe168
Anez <31077044+alasal17@users.noreply.github.com>
2022-10-07 20:25:31 +0200

    fix package versions

--- src/pages/single/Calenders.jsx
+++ src/pages/single/Calenders.jsx
@@ -359,7 +359,7 @@ getEvents(){
     return (
       <div className="new">
         <iframe
-        src="https://calendar.google.com/calendar/embed?src=minbil%group.calendar.google.com&ctz=Norway%2FOslo"
+        src="https://calendar.google.com/calendar/embed?src=mnlbc4vk9vsouso942iti2geec%40group.calendar.google.com&ctz=America%2FMexico_City"
         style={{ border: "0" }}
         width="800"
         height="600"
