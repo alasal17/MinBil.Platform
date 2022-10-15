edb64cbb2248ec8b382a5afa2f0451801af5dfa1
Anez <salam@stimedia.no>
2022-10-15 02:38:48 +0200

    d

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,9 @@ FROM node:16
 
 COPY package.json .
 
-<<<<<<< Updated upstream
+
 RUN npm install
-=======
-RUN npm install --legacy-peer-deps
 
-COPY . .
-
-
-CMD [ "npm run dev" ]
->>>>>>> Stashed changes
 
 COPY . .
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-<<<<<<< Updated upstream
+
 import React from "react";
-=======
-import React from 'react';
->>>>>>> Stashed changes
+
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
