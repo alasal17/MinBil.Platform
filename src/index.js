d74dbcf3111429ebf26d6e9f5722c1f113f721d4
Anez <salam@stimedia.no>
2022-12-08 23:12:58 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-
+import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { DarkModeContextProvider } from "./context/darkModeContext";
-import { createRoot } from "react-dom/client";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
 
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <DarkModeContextProvider>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </DarkModeContextProvider>
-  </React.StrictMode>
-  
+  </React.StrictMode>,
+  document.getElementById("root")
 );
