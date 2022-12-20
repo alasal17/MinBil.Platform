fee7b894feac269782a8fcc2d57e2c229ee462ab
Anez <31077044+alasal17@users.noreply.github.com>
2022-12-20 05:21:48 +0100

    Create MultipleStepFormPopup.js

---
+++ src/components/popup/MultipleStepFormPopup.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+function RegistrationForm() {
+  const [show, setShow] = useState(false);
+  const [page, setPage] = useState(1);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleNextPage = () => setPage(page + 1);
+  const handlePrevPage = () => setPage(page - 1);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Open Form
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        {page === 1 && (
+          <>
+            <Modal.Header closeButton>
+              <Modal.Title>Registration Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {/* First page of the form */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleNextPage}>
+                Next
+              </Button>
+            </Modal.Footer>
+          </>
+        )}
+        {page === 2 && (
+          <>
+            <Modal.Header closeButton>
+              <Modal.Title>Registration Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {/* Second page of the form */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handlePrevPage}>
+                Previous
+              </Button>
+              <Button variant="primary" onClick={handleNextPage}>
+                Next
+              </Button>
+            </Modal.Footer>
+          </>
+        )}
+        {page === 3 && (
+          <>
+            <Modal.Header closeButton>
+              <Modal.Title>Registration Form</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {/* Third page of the form */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handlePrevPage}>
+                Previous
+              </Button>
+              <Button variant="primary" onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Modal.Footer>
+          </>
+        )}
+      </Modal>
+    </>
+  );
+}
